278ddb4e5fba827df2c70623c8b25b8f1cc6e9d3
OOO <kfmk>
2015-01-19 15:53:16 +0700

    FINISH

--- app/src/main/java/th/ac/tu/siit/cholwich/lab1exercise2/MainActivity.java
+++ app/src/main/java/th/ac/tu/siit/cholwich/lab1exercise2/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -36,4 +40,61 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void converter(View v){
+        EditText etInput = (EditText)findViewById(R.id.etInput);
+        TextView tvOutput = (TextView)findViewById(R.id.tvOutput);
+        String s = etInput.getText().toString();
+
+        double input = Double.parseDouble(s);
+
+
+        //double input = Double.parseDouble(s);
+
+        RadioGroup rgFrom = (RadioGroup)findViewById(R.id.rgFrom);
+        int selFrom = rgFrom.getCheckedRadioButtonId();
+        RadioGroup rgTo = (RadioGroup)findViewById(R.id.rgTo);
+        int selTo = rgTo.getCheckedRadioButtonId();
+
+        if (selFrom == R.id.rbFrmC && selTo == R.id.rbToF) { //The user want to convert from celsius
+          tvOutput.setText((input*(5/9) + 32)+"");
+
+        }
+        if (selFrom == R.id.rbFrmC && selTo == R.id.rbToK) { //The user want to convert from celsius
+            tvOutput.setText((input + 273.15)+"");
+
+        }
+        if ((selFrom == R.id.rbFrmC && selTo == R.id.rbToC) ||
+           (selFrom == R.id.rbFrmF && selTo == R.id.rbToF) ||
+           (selFrom == R.id.rbFrmK && selTo == R.id.rbToK)){ //The user want to convert from celsius
+            tvOutput.setText((input)+"");
+
+        }
+        if (selFrom == R.id.rbFrmF && selTo == R.id.rbToC) { //The user want to convert from celsius
+            tvOutput.setText((input-32) *(5/9)+"");
+
+        }
+        if (selFrom == R.id.rbFrmF && selTo == R.id.rbToK) { //The user want to convert from celsius
+            tvOutput.setText((input+459.67)*(5/9)+"");
+
+        }
+
+        if (selFrom == R.id.rbFrmK && selTo == R.id.rbToC) { //The user want to convert from celsius
+            tvOutput.setText((input-273.15)+"");
+
+        }if (selFrom == R.id.rbFrmK && selTo == R.id.rbToF) { //The user want to convert from celsius
+            tvOutput.setText((input)*(5/9)-(459.67)+"");
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
 }
